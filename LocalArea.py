f27bb5abbebd6dbf91945f680b720cdb04cb9e7f
rutayisire24 <rutayisire24meddy@gmail.com>
2024-03-16 08:58:17 +0300

    Intial Commit
    The App performs data quality checks  on the uploaded file.

---
+++ LocalArea.py
@@ -0,0 +1,210 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from adtk.detector import QuantileAD
+import io
+import base64
+
+mfl = pd.read_excel('mfl.xlsx')
+## Function 
+
+#@st.cache_data
+def detect_outliers(data, column_name):
+
+
+    data = data[[column_name, 'organisationunitname']].copy()
+
+    # Placeholder for the results
+    outlier_results = []
+
+    for unit in data['organisationunitname'].unique():
+        print('processing unit:', unit)
+
+        sample_fac_data = data[data['organisationunitname'] == unit]
+        period_names = sample_fac_data.index
+
+        try:
+            # Data validation and imputation
+            data_test = sample_fac_data.drop('organisationunitname', axis=1)
+            data_test = validate_series(data_test)
+            quantile_ad = QuantileAD(high=0.999999999, low=0.0000001)
+            anomaly_scores = quantile_ad.fit_detect(data_test) 
+            # Create results DataFrame
+            anomalies = pd.DataFrame(index=period_names)
+            anomalies['organisationunitname'] = unit
+            anomalies[column_name] = data_test[column_name]  
+            anomalies['outlier'] = anomaly_scores != 0
+            anomalies['missing'] = data_test[column_name].isna()
+            outlier_results.append(anomalies)
+
+        except Exception as e:
+            print(f"Error during processing for unit {unit}: {e}. Skipping this unit.")
+            continue
+
+    # Concatenate results
+    all_outliers = pd.concat(outlier_results, ignore_index=False)
+        # New code to aggregate outlier counts per facility
+    outlier_counts = all_outliers.groupby('organisationunitname')['outlier'].sum().reset_index()
+    outlier_counts.columns = ['Facility', 'Outlier Count']
+    outlier_counts.sort_values('Outlier Count', ascending=False, inplace=True)
+    
+    return all_outliers , outlier_counts
+
+def validate_series(data_test):
+    """Validates the data and handles missing values.
+    """
+    # Other validation checks (ensure data type, etc.)
+    #data_test = data_test.fillna(data_test.mean())  # Impute missing values with the mean
+    #return data_test
+
+    # Concatenate results
+    all_outliers = pd.concat(outlier_results, ignore_index=False)
+    all_outliers = pd.merge(all_outliers,mfl , left_on= "organisationunitname", right_on='facility' , how= 'outer')
+        # New code to aggregate outlier counts per facility
+    outlier_counts = all_outliers.groupby('organisationunitname')['outlier'].sum().reset_index()
+    outlier_counts.columns = ['Facility','Outlier Count']
+    outlier_counts.sort_values('Outlier Count', ascending=False, inplace=True)
+    
+    return all_outliers , outlier_counts
+
+def validate_series(data_test):
+    """Validates the data and handles missing values.
+    """
+    # Other validation checks (ensure data type, etc.)
+    data_test = data_test.fillna(data_test.median())  # Impute missing values with the mean
+    return data_test
+
+# Streamlit app layout
+st.title('HMIS - Data Quality App')
+
+with st.expander("How to Use This App"):
+    st.write("""
+    **Purpose:** This app helps you identify potential outliers in your time series data based on statistical analysis. 
+
+    **Steps:**
+    1. **Upload CSV File:** Click the "Choose a CSV file" button and select the file. Your data should have a 'periodname' column (as dates) and a 'organisationunitname' column.
+    2. **Select Data Element:** Choose the column you want to analyze from the dropdown.
+    3. **Review Results:** The app will process your data and generate:
+       * A line chart visualizing the data points and highlighting potential outliers (red markers).
+       * A count of possible outliers for the selected facility.
+    4. **Filter by Facility:** Use the facility dropdown to focus on results for a specific facility.
+    5. **Download Outliers:**  Click the "Download as CSV" link to save identified outliers.
+    """)
+
+st.subheader("Upload Data to be Accessed")
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    with st.spinner("Processing data..."):
+        try:
+            data = pd.read_csv(uploaded_file, index_col='periodname', parse_dates=True)
+            st.success("CSV file uploaded successfully!")
+            st.write('Preview of the Uploaded Data')
+            st.write(data.head())
+            # ... (The rest of your app code, using the 'data' variable)
+
+        except Exception as e:
+            st.error(f"Error uploading CSV file: {e}")
+
+## Select the column to Display 
+if uploaded_file is not None:
+  columns = data.columns
+  selected_col = st.selectbox("Select a Data element" , columns) 
+
+  ## run the function on the selected columns 
+
+  outliers_df, outlier_counts = detect_outliers(data.copy() , selected_col)  # Make a copy to avoid modifying the original data
+  data = outliers_df.copy()
+  data.index = pd.to_datetime(data.index)
+
+  ## try somethings 
+  total_facilities = len(data['organisationunitname'].unique())
+
+  # Calculate the number of facilities with at least one outlier
+  facilities_with_outliers = outlier_counts[outlier_counts['Outlier Count'] > 0].shape[0]
+
+  # Compute the percentage of facilities with at least one outlier
+  percentage_with_outliers = (facilities_with_outliers / total_facilities) * 100
+  
+# show possible outliers 
+  outlier_counts = pd.merge(outlier_counts,mfl , left_on= "Facility", right_on='facility' , how= 'outer')
+  outlier_counts = (outlier_counts.drop('facility' , axis = 1)).dropna() 
+  st.subheader("Possible Outlier Counts by Facility")
+  st.write(outlier_counts)
+
+  # Display the metrics
+  # Column layout setup
+  cols = st.columns(3)  # Create three columns
+
+  # Metrics in columns
+  with cols[0]:
+      st.metric(label="Total Facilities Reviewed", value=total_facilities)
+
+  with cols[1]:
+      st.metric(label="Facilities with Potential Outliers", value=facilities_with_outliers)
+
+  with cols[2]:
+      st.metric(label="%  Potential Outliers", value=f"{percentage_with_outliers:.2f}%")
+
+  # Download CSV section
+  st.subheader("Download Results")
+  if outliers_df.shape[0] > 0:  # Check if there are outliers to download
+      csv = outliers_df.to_csv(index=False)
+      b64 = base64.b64encode(csv.encode()).decode()  
+      href = f'<a href="data:file/csv;base64,{b64}" download="outliers.csv">Download as CSV</a>'
+      st.markdown(href, unsafe_allow_html=True)
+  else:
+      st.warning("No outliers found to download.")
+  st.subheader("Explore the Data")
+    # Dropdown to select facility
+  facilities_with_outliers = data[data['outlier'] == True]['organisationunitname'].unique()
+  selected_facility = st.selectbox('Select a Facility', facilities_with_outliers)
+
+  # Filter data based on selection
+  filtered_data = data[data['organisationunitname'] == selected_facility]
+  
+  missing_counts = filtered_data.groupby('organisationunitname')['missing'].sum().reset_index()
+  missing_counts.columns = ['Facility', 'Counts of Missing Values']
+  
+  st.write( f'Missing Values for {selected_facility} in {selected_col}')
+  st.write(missing_counts)
+  
+  # Count the number of possible outliers for the selected facility
+  num_outliers = filtered_data['outlier'].sum()
+  # Display the summary at the top as a value box
+  st.metric(label="Number of Possible Outliers", value=num_outliers)
+  # Plotting with Plotly
+      # Plotting with Plotly
+  fig = go.Figure()
+
+  # Add line for the data
+  fig.add_trace(go.Scatter(x=filtered_data.index, y=filtered_data[selected_col], mode='lines', name='Data'))
+
+  # Add markers for outliers and non-outliers
+  outliers = filtered_data['outlier'] == True
+  fig.add_trace(go.Scatter(x=filtered_data[outliers].index, y=filtered_data[outliers][selected_col], 
+                            mode='markers', marker=dict(color='red'), name='Outlier'))
+  fig.add_trace(go.Scatter(x=filtered_data[~outliers].index, y=filtered_data[~outliers][selected_col], 
+                            mode='markers', marker=dict(color='blue'), name='Not Outlier'))
+
+  # Calculate quantiles (you can adjust the values as needed)
+  q1 = filtered_data[selected_col].quantile(0.01)
+  q3 = filtered_data[selected_col].quantile(0.99)
+
+  # Add quantile lines for visual reference
+  fig.add_shape(type='line', y0=q1, y1=q1, x0=filtered_data.index.min(), x1=filtered_data.index.max(),
+                line=dict(color='yellow', width=2, dash='dash'), name='25th Quantile')
+  fig.add_shape(type='line', y0=q3, y1=q3, x0=filtered_data.index.min(), x1=filtered_data.index.max(),
+                line=dict(color='cyan', width=2, dash='dash'), name='75th Quantile')
+
+  # Update layout
+  fig.update_layout(title=f'Possible Outliers for {selected_facility} in  <br> {selected_col}', 
+                    xaxis_title='', yaxis_title='', legend_title='Data Points',
+                    showlegend=True)  
+
+ 
+  # Show plot in Streamlit
+  st.plotly_chart(fig) 
+
+   # Short explanation about quantiles
+  st.write("**Quantiles:** The yellow and cyan lines represent the 10th and 99th quantiles respectively, helping you see how the data is distributed around the central values.")
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import numpy as np
 from scipy import stats
 
 st.title("Dataset Quality Checker")
-st.write("This application will allow you to upload your dataset and run a quality check on it.")
+st.write(
+    "This application will allow you to upload your dataset and run a quality check on it."
+)
 st.markdown("---")
+
 
 def identify_missing_values(data):
     missing_flags = data.isnull()
@@ -19,9 +22,9 @@ def identify_missing_values(data):
 # Uploading the dataset
 st.subheader("Upload your files here : ")
 
-upload_data = st.file_uploader("Choose a CSV file", type = ['CSV'])
+upload_data = st.file_uploader("Choose a CSV file", type=["CSV"])
 if upload_data is not None:
-    read_data = pd.read_csv(upload_data, encoding='latin-1',on_bad_lines='skip')
+    read_data = pd.read_csv(upload_data, encoding="latin-1", on_bad_lines="skip")
 
 
 # Looking at your dataset
@@ -36,7 +39,9 @@ except:
 st.subheader("Select Columns for Quality Checks:")
 selected_columns = st.multiselect("Choose columns", read_data.columns.tolist())
 if not selected_columns:
-    st.warning("No columns selected. Please select at least one column to proceed with quality checks.")
+    st.warning(
+        "No columns selected. Please select at least one column to proceed with quality checks."
+    )
     st.stop()
 else:
     # Continue with your data quality checks on the selected columns
@@ -56,71 +61,91 @@ st.markdown("---")
 # More Exploration
 st.subheader(" Dataset Quality Checks:")
 
-#Checking for Null Values : 
-if st.button('Missing Values Check'):
+# Checking for Null Values :
+if st.button("Missing Values Check"):
     data, summary, violations = identify_missing_values(read_data)
     st.write(f"Missing values summary:\n{summary}")
-    
+
     if not violations.empty:
-        csv = violations.to_csv(index=False).encode('utf-8')
+        csv = violations.to_csv(index=False).encode("utf-8")
         st.download_button(
             label="Download Flagged Records for Missing Values",
             data=csv,
             file_name="missing_values.csv",
-            mime='text/csv',
+            mime="text/csv",
         )
 
-#Check for completeness ratio
-if st.button("Check for Completeness Ratio",key=3):
-    not_missing = (read_data.notnull().sum().round(2))
-    completeness = round(sum(not_missing)/len(read_data),2)
-    st.write("Completeness Ratio for the dataset : ",completeness)
-    if completeness >=0.80:
-        st.success('Looks Good !')
+# Check for completeness ratio
+if st.button("Check for Completeness Ratio", key=3):
+    not_missing = read_data.notnull().sum().round(0)
+    completeness = round(sum(not_missing) / len(read_data), 0)
+    st.write("Completeness Ratio for the dataset : ", completeness)
+    if completeness >= 0.80:
+        st.success("Looks Good !")
     else:
-        st.error('Poor Data Quality due to low completeness ratio : less than 80 perecent !')
-        st.text('Completeness is defined as the ratio of non-missing values to total records in dataset.')
+        st.error(
+            "Poor Data Quality due to low completeness ratio : less than 80 perecent !"
+        )
+        st.text(
+            "Completeness is defined as the ratio of non-missing values to total records in dataset."
+        )
+
 
 # Function to flag outliers using IQR
 def flag_outliers_iqr(group):
-    q1 = group['Value'].quantile(0.25)
-    q3 = group['Value'].quantile(0.75)
+    q1 = group["Value"].quantile(0.25)
+    q3 = group["Value"].quantile(0.75)
     iqr = q3 - q1
     lower_bound = q1 - 1.5 * iqr
     upper_bound = q3 + 1.5 * iqr
-    group['possible_outlier'] = ['possible' if (x < lower_bound or x > upper_bound) else 'no' for x in group['Value']]
+    group["possible_outlier"] = [
+        "possible" if (x < lower_bound or x > upper_bound) else "no"
+        for x in group["Value"]
+    ]
     return group
+
 
 # Adjusted Outlier Check for Facility Level using IQR
 if st.button("Check for Outliers by Facility", key="facility_outliers_iqr"):
     # Convert selected data to long format for outlier analysis
-    long_df = pd.melt(read_data, id_vars= ['organisationunitname'], var_name='Metric', value_name='Value')
+    long_df = pd.melt(
+        read_data,
+        id_vars=["organisationunitname"],
+        var_name="Metric",
+        value_name="Value",
+    )
 
-    
     # Apply the outlier detection and flagging, grouped by facility and Metric
-    flagged_df = long_df.groupby(['organisationunitname', 'Metric'], as_index=False).apply(flag_outliers_iqr)
-    
-    # Filter to obtain only the rows flagged as possible outliers
-    outliers_df = flagged_df[flagged_df['possible_outlier'] == 'possible']
+    flagged_df = long_df.groupby(
+        ["organisationunitname", "Metric"], as_index=False
+    ).apply(flag_outliers_iqr)
 
-    outliers_df = pd.pivot_table(outliers_df,index="organisationunitname", columns='Metric', values='Value')
-    final_outliers = pd.merge(outliers_df,read_data , how='left' )
-    
+    # Filter to obtain only the rows flagged as possible outliers
+    outliers_df = flagged_df[flagged_df["possible_outlier"] == "possible"]
+
+    outliers_df = pd.pivot_table(
+        outliers_df, index="organisationunitname", columns="Metric", values="Value"
+    )
+    final_outliers = pd.merge(outliers_df, read_data, how="left")
 
     if not outliers_df.empty:
-        st.warning("Found outliers in your dataset, grouped by facility. Here are some of them:")
+        st.warning(
+            "Found outliers in your dataset, grouped by facility. Here are some of them:"
+        )
         st.dataframe(outliers_df.head())  # Show a preview of outliers
 
         # Optionally, provide a download button for the outlier data
-        csv = outliers_df.to_csv(index=False).encode('utf-8')
+        csv = outliers_df.to_csv(index=False).encode("utf-8")
         st.download_button(
             label="Download Flagged Outliers by Facility as CSV",
             data=csv,
             file_name="flagged_outliers_by_facility.csv",
-            mime='text/csv',
+            mime="text/csv",
         )
     else:
-        st.success("No significant outliers found in the selected columns by facility using IQR.")
-st.markdown('---')
+        st.success(
+            "No significant outliers found in the selected columns by facility using IQR."
+        )
+st.markdown("---")
 
-st.subheader('> Thank you for using the dataset quality checker.')
+st.subheader("> Thank you for using the dataset quality checker.")
